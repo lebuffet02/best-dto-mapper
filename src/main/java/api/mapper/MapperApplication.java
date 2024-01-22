46ca427182f52fa675579885b61a98249405cd21
Lucas Buffet <lebuffet02@gmail.com>
2024-01-22 02:47:21 -0300

    base

---
+++ src/main/java/api/mapper/MapperApplication.java
@@ -0,0 +1,14 @@
+package api.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class MapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperApplication.class, args);
+	}
+}
